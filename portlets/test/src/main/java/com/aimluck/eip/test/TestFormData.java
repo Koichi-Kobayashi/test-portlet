484a368900be687823175287824d293b6459916b
tomoumi-n <nishimura@aimluck.com>
2015-02-26 15:41:11 +0900

    url追加

--- portlets/test/src/main/java/com/aimluck/eip/test/TestFormData.java
+++ portlets/test/src/main/java/com/aimluck/eip/test/TestFormData.java
@@ -36,6 +36,7 @@ import org.apache.velocity.context.Context;
 import com.aimluck.commons.field.ALDateField;
 import com.aimluck.commons.field.ALNumberField;
 import com.aimluck.commons.field.ALStringField;
+import com.aimluck.commons.field.ALUrlField;
 import com.aimluck.eip.cayenne.om.portlet.EipTTest;
 import com.aimluck.eip.cayenne.om.security.TurbineUser;
 import com.aimluck.eip.common.ALAbstractFormData;
@@ -86,6 +87,9 @@ public class TestFormData extends ALAbstractFormData {
 
   /** メモ */
   private ALStringField note;
+
+  /** URL */
+  private ALStringField url;
 
 
   /** 現在の年 */
@@ -179,6 +183,10 @@ public class TestFormData extends ALAbstractFormData {
     note.setFieldName(ALLocalizationUtils.getl10n("TODO_SETFIELDNAME_MEMO"));
     note.setTrim(false);
 
+    // URL
+    url = new ALStringField();
+    url.setFieldName(ALLocalizationUtils.getl10n("TEST_URL"));
+    url.setTrim(true);
   }
 
 
@@ -220,6 +228,8 @@ public class TestFormData extends ALAbstractFormData {
     test_name.validate(msgList);
     // メモ
     note.validate(msgList);
+    // URL
+    url.validate(msgList);
 
     return (msgList.size() == 0);
 
@@ -247,6 +257,8 @@ public class TestFormData extends ALAbstractFormData {
       test_name.setValue(test.getTestName());
       // メモ
       note.setValue(test.getNote());
+       // url
+      url.setValue(test.getUrl());
 
 
       // 担当者
@@ -328,6 +340,8 @@ public class TestFormData extends ALAbstractFormData {
 
       // メモ
       test.setNote(note.getValue());
+      // URL
+      test.setUrl(url.getValue());
       // 作成日
       test.setCreateDate(Calendar.getInstance().getTime());
       // 更新日
@@ -396,6 +410,8 @@ public class TestFormData extends ALAbstractFormData {
       test.setTurbineUser(tuser);
       // メモ
       test.setNote(note.getValue());
+      // URL
+      test.setUrl(url.getValue());
       // 更新日
       test.setUpdateDate(Calendar.getInstance().getTime());
       // Test を更新
@@ -452,6 +468,15 @@ public class TestFormData extends ALAbstractFormData {
    */
   public ALStringField getTestName() {
     return test_name;
+  }
+
+  /**
+   * URLを取得します。 <BR>
+   *
+   * @return
+   */
+  public ALStringField getTestURL() {
+    return url;
   }
 
 
