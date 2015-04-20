61fa597a2aa0d831d724528c32d989f3c43d64a9
cojozzz <m.i.l.l.wancan@gmail.com>
2015-04-20 15:55:54 +0900

    項目追加しました

--- portlets/test/src/main/java/com/aimluck/eip/test/TestFormData.java
+++ portlets/test/src/main/java/com/aimluck/eip/test/TestFormData.java
@@ -88,6 +88,9 @@ public class TestFormData extends ALAbstractFormData {
   private ALStringField note;
 
 
+  private ALStringField url;
+
+
   /** 現在の年 */
   private int currentYear;
 
@@ -179,6 +182,10 @@ public class TestFormData extends ALAbstractFormData {
     note.setFieldName(ALLocalizationUtils.getl10n("TODO_SETFIELDNAME_MEMO"));
     note.setTrim(false);
 
+    // URL
+    url = new ALStringField();
+    url.setFieldName(ALLocalizationUtils.getl10n("REGISTRY_EDITOR_PORTLET_URL"));
+    url.setTrim(false);
   }
 
 
@@ -247,8 +254,8 @@ public class TestFormData extends ALAbstractFormData {
       test_name.setValue(test.getTestName());
       // メモ
       note.setValue(test.getNote());
-
-
+      // URL
+      url.setValue(test.getUrl());
       // 担当者
       user_id.setValue(test.getTurbineUser().getUserId());
 
@@ -328,6 +335,8 @@ public class TestFormData extends ALAbstractFormData {
 
       // メモ
       test.setNote(note.getValue());
+      // URL
+      test.setUrl(url.getValue());
       // 作成日
       test.setCreateDate(Calendar.getInstance().getTime());
       // 更新日
@@ -396,6 +405,8 @@ public class TestFormData extends ALAbstractFormData {
       test.setTurbineUser(tuser);
       // メモ
       test.setNote(note.getValue());
+      // URL
+      test.setNote(url.getValue());
       // 更新日
       test.setUpdateDate(Calendar.getInstance().getTime());
       // Test を更新
@@ -432,6 +443,16 @@ public class TestFormData extends ALAbstractFormData {
     }
 
     return true;
+  }
+
+
+  /**
+   * URLを取得します。<BR>
+   *
+   * @return
+   */
+  public ALStringField getUrl() {
+	  return url;
   }
 
 
