e3700f5f583025568d75e66736c588b909c54d4a
Yudais <ninninjajaja12@gmail.com>
2015-10-13 16:00:52 +0900

    サムネイル完成、ドメイン抜き出し途中

--- portlets/test/src/main/java/com/aimluck/eip/test/TestResultData.java
+++ portlets/test/src/main/java/com/aimluck/eip/test/TestResultData.java
@@ -20,6 +20,8 @@
 package com.aimluck.eip.test;
 
 import java.util.Date;
+import java.net.URI;
+import java.net.URL;
 
 import com.aimluck.commons.field.ALDateTimeField;
 import com.aimluck.commons.field.ALNumberField;
@@ -141,6 +143,11 @@ public class TestResultData implements ALData {
   
   public String getUrlThum() {
 	  return url.getValue();
+  }
+  
+  public String getUrlDomain() throws Exception {
+	  URL u = new URI(url.getValue());
+	  return u.getScheme()+"://"+u.getHost();
   }
 
   /**
